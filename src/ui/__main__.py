cf875d14a2ae89c129ff4ef42b4015d92b47991c
Aleksandar <atodorovic@student.tgm.ac.at>
2026-02-17 09:13:00 +0100

    flask app starten gui

--- src/ui/__main__.py
+++ src/ui/__main__.py
@@ -1,4 +1,6 @@
-from . import main
+from .main import create_app
 
+# allow starting the web server with “python -m src.ui”
 if __name__ == "__main__":
-    main()
+    # create and run the flask app directly
+    create_app().run(debug=True)
